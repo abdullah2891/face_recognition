3b05fb19b0c59b147f2fb299cd56bef33f6acdfc
abdullah2891 <abdullah2891@gmail.com>
2015-10-20 14:10:25 +0600

    tested with different images

--- input_image.py
+++ input_image.py
@@ -11,13 +11,15 @@ __author__ = 'Abdullah_Rahman'
 #r=kath
 threshold=0.55
 
-filenames=("caprio.jpg","guy.jpg")
+
+
+filenames=("caprio.jpg","aron.jpg")
 color=('r','b')
 cascade="frontalface.xml"
 C=KMeans(n_clusters=4)
 face_cascade=cv2.CascadeClassifier(cascade)
 
-image=cv2.imread("tes1.jpg",0)
+image=cv2.imread("aronTest.jpg",0)
 features=Feature_extractor(image)
 
 face=face_cascade.detectMultiScale(
@@ -51,17 +53,20 @@ print "counting frequency"
 
 l= list(points)
 
-Kath=list(l).count('r')
-Reich=list(l).count('b')
-Kath_percentage= float(Kath)/float(Kath+Reich)
-Reich_percentage=float(Reich)/float(Kath+Reich)
+first_image=list(l).count('r')
+second_image=list(l).count('b')
+first_percentage= float(first_image)/float(first_image+second_image)
+second_percentage=float(second_image)/float(first_image+second_image)
 
-print Kath_percentage,Reich_percentage
-if Kath_percentage>threshold:
+
+print "confidence of first image/confidence of Second Image"
+print first_percentage,second_percentage
+if first_percentage>threshold:
     print filenames[0]+" IS THAT YOU!?"
 
-if Reich_percentage>threshold:
+if second_percentage>threshold:
     print filenames[1]+",Hi"
+
 
 
 
