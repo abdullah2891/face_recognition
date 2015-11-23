ff7c58e8b4455dee5658669f9291fa10b9c21704
abdullah2891 <abdullah2891@gmail.com>
2015-11-23 17:36:46 +0600

    .

---
+++ recognition.py
@@ -0,0 +1,9 @@
+import pickle
+
+__author__ = 'Abdullah_Rahman'
+
+
+
+keypoints_caprio = pickle.load(open("caprio_keypoints.pkl","rb"))
+keypoints_aron = pickle.load(open("aron_keypoints.pkl","rb"))
+
