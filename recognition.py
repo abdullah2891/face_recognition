d46beb88a93c9babc8147bdff0225814c3a58efc
abdullah2891 <abdullah2891@gmail.com>
2015-11-23 17:47:40 +0600

    .

--- recognition.py
+++ recognition.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 __author__ = 'Abdullah_Rahman'
 
@@ -7,3 +8,7 @@ __author__ = 'Abdullah_Rahman'
 keypoints_caprio = pickle.load(open("caprio_keypoints.pkl","rb"))
 keypoints_aron = pickle.load(open("aron_keypoints.pkl","rb"))
 
+C = np.asarray(keypoints_aron.features_())
+A = np.asarray(keypoints_caprio.features_())
+
+print C
