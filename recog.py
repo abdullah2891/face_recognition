6c226fa6783915c2efcb42b3c0192b7f330a0733
abdullah2891 <abdullah2891@gmail.com>
2015-12-03 13:07:16 +0600

    working on recognition

--- recog.py
+++ recog.py
@@ -1,15 +1,24 @@
 import pickle
 import numpy as np
-
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 __author__ = 'Abdullah_Rahman'
 
 def separate(C,y_threshold,x_threshold):
-    bottom =C[np.where(C[:,1]<y_threshold)]
-    right = C[np.where((C[:,0]<x_threshold) & (C[:,1]>y_threshold))]
-    left = C[np.where((C[:,0]>x_threshold) & (C[:,1]>y_threshold))]
+    bottom =C[np.where(C[:,1]>y_threshold)]
+    right = C[np.where((C[:,0]<x_threshold) & (C[:,1]<y_threshold))]
+    left = C[np.where((C[:,0]>x_threshold) & (C[:,1]<y_threshold))]
     segmented = {'bottom': bottom, 'right_eye':right,'left_eye':left}
 
     return segmented
+
+def clustering(A,number_cluster):
+    C = KMeans(n_clusters=number_cluster)
+    C.fit(A)
+    return C
+
+def distance(A,reference,axis):
+    return
 
 
 
@@ -22,4 +31,11 @@ feature_caprio = np.asarray(caprio.features_())
 feature_aron = np.asarray(aron.features_())
 
 
-print separate(feature_caprio,y_threshold=50,x_threshold=50)
+segmented = separate(feature_caprio,y_threshold=50,x_threshold=50)
+
+
+plt.scatter(segmented['left_eye'][:,0],segmented['left_eye'][:,1],color='g')
+plt.scatter(segmented['bottom'][:,0],segmented['bottom'][:,1],color='r')
+plt.scatter(segmented['right_eye'][:,0],segmented['right_eye'][:,1],color='b')
+
+plt.show()
